504e9f1a8338a296f6a551994188f27a0465c714
amuxue <594093246@qq.com>
2021-03-20 20:42:03 +0800

    修改验证点

--- test_addmember.py
+++ test_addmember.py
@@ -16,27 +16,25 @@ class TestAddMemeber:
         caps["appActivity"] = ".launch.WwMainActivity"
         caps["noReset"] = "true"
         caps["automationName"] = "uiautomator2"
+        caps["ensureWebviewsHavePages"] = True
+        # 设置页面等待空闲状态的时间为0秒
+        caps['settings[waitForIdleTimeout]'] = 1
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', caps)
         self.driver.implicitly_wait(3)
-
 
     def teardown(self):
         self.driver.quit()
 
     def test_open(self):
         self.driver.find_element(MobileBy.XPATH,'//*[contains(@text,"通讯录")]').click()
-
         self.driver.find_element(MobileBy.XPATH, '//*[@text="添加成员"]/parent::*/parent::*/parent::*/parent::*').click()
         self.driver.find_element(MobileBy.XPATH, '//*[@resource-id="com.tencent.wework:id/cth"]').click()
-        self.driver.find_element(MobileBy.XPATH, '//*[contains(@text,"姓名")]/parent::*//*[@class="android.widget.EditText"]').send_keys("啊紫")
-        self.driver.find_element(MobileBy.XPATH,'//*[contains(@text,"手机")]/parent::*//*[@class="android.widget.EditText"]').send_keys("14444444443")
+        self.driver.find_element(MobileBy.XPATH, '//*[contains(@text,"姓名")]/parent::*//*[@class="android.widget.EditText"]').send_keys("啊紫2")
+        self.driver.find_element(MobileBy.XPATH,'//*[contains(@text,"手机")]/parent::*//*[@class="android.widget.EditText"]').send_keys("14444444445")
         self.driver.find_element(MobileBy.XPATH,
                                  '//*[@resource-id="com.tencent.wework:id/aj_"]').click()
         sleep(3)
-       # result=self.driver.find_element(MobileBy.XPATH,
-       #                           '//*[@class="android.widget.Toast"]').text
-        # 获取toast提示框内容
-        # toast_element = WebDriverWait(self.driver, 10).until(lambda x: x.find_element_by_xpath('//*[@text="添加成功"]'))
-        # print(toast_element.text)
-        # assert toast_element.text == "添加成功"
-        # sleep(3)
+        result=self.driver.find_element(MobileBy.XPATH,
+                                 '//*[@class="android.widget.Toast"]').text
+        print(result)
+        assert result== "添加成功"
